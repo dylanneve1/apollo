332075f3f10597ab41d7d88ccfed352f52cd668e
Anan Jaser <ananjaser@gmail.com>
2022-11-21 23:11:15 +0400

    Apollo: Mark version

--- apollo.sh
+++ apollo.sh
@@ -35,7 +35,7 @@ CR_KERNEL=$CR_DIR/arch/arm64/boot/Image
 CR_DTB=$CR_DIR/arch/arm64/boot/dtb.img
 # Kernel Name and Version
 CR_VERSION=V3.2
-CR_NAME=Apollo
+CR_NAME=Apollo_QHWC
 # Thread count
 CR_JOBS=$(nproc --all)
 # Target Android version
